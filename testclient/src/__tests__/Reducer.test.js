bb8066704767dd5d1e3c9beb914c84b8561a7a33
xu <yongchengxu@gmail.com>
2019-07-29 19:17:58 +1200

    no message

--- testclient/src/__tests__/Reducer.test.js
+++ testclient/src/__tests__/Reducer.test.js
@@ -1,190 +1,40 @@
-import { MoviesReducer } from '../reducers'
-
-var movies=[
+import { MoviesReducer } from '../reducer'
+import { addFilter } from '../actionTypes';
+var movies = [
   {
     id: '10',
-    title:'A Very English Scandal',
+    title: 'A Very English Scandal',
     akas: ["Egy nagyon angolos botr", "Un scandal britanic"],
-    genres: [  "Drama", "Legal"],
-    image: [ "http://static.tvmaze.com/uploads/images/medium_portrait/164/411209.jpg", "http://static.tvmaze.com/uploads/images/original_untouched/164/411209.jpg"],
+    genres: ["Drama", "Legal"],
+    image: ["http://static.tvmaze.com/uploads/images/medium_portrait/164/411209.jpg", "http://static.tvmaze.com/uploads/images/original_untouched/164/411209.jpg"],
     rating: 7,
-    lang:'English',
+    lang: 'English',
     timestamp: '2019/7/21  1:07:53'
-  },
-  {
-      id: '20',
-      title:'A Very English Scandal',
-      akas: ["Egy nagyon angolos botr", "Un scandal britanic"],
-      genres: [ "Drama", "Legal"],
-      image: [ "http://static.tvmaze.com/uploads/images/medium_portrait/164/411209.jpg", "http://static.tvmaze.com/uploads/images/original_untouched/164/411209.jpg"],
-      rating: 7,
-      lang:'English',
-      timestamp: '2019/7/21  1:07:53'
-  },
-  {
-      id: '30',
-      title:'A Very English Scandal',
-      akas: ["Egy nagyon angolos botr", "Un scandal britanic"],
-      genres: [ "Adventure", "Drama"],
-      image: [ "http://static.tvmaze.com/uploads/images/medium_portrait/164/411209.jpg", "http://static.tvmaze.com/uploads/images/original_untouched/164/411209.jpg"],
-      rating: 7,
-      lang:'English',
-      timestamp: '2019/7/21  1:07:53'
-  },
-  {
-      id: '40',
-      title:'A Very English Scandal',
-      akas: ["Egy nagyon angolos botr", "Un scandal britanic"],
-      genres: [  "Comedy" ],
-      image: [ "http://static.tvmaze.com/uploads/images/medium_portrait/164/411209.jpg", "http://static.tvmaze.com/uploads/images/original_untouched/164/411209.jpg"],
-      rating: 7,
-      lang:'English',
-      timestamp: '2019/7/21  1:07:53'
-    },
-    {
-        id: '50',
-        title:'A Very English Scandal',
-        akas: ["Egy nagyon angolos botr", "Un scandal britanic"],
-        genres: [ "Adventure", "Drama"],
-        image: [ "http://static.tvmaze.com/uploads/images/medium_portrait/164/411209.jpg", "http://static.tvmaze.com/uploads/images/original_untouched/164/411209.jpg"],
-        rating: 7,
-        lang:'English',
-        timestamp: '2019/7/21  1:07:53'
-    },
-    {
-        id: '60',
-        title:'A Very English Scandal',
-        akas: ["Egy nagyon angolos botr", "Un scandal britanic"],
-        genres: [ "Adventure", "Drama"],
-        image: [ "http://static.tvmaze.com/uploads/images/medium_portrait/164/411209.jpg", "http://static.tvmaze.com/uploads/images/original_untouched/164/411209.jpg"],
-        rating: 7,
-        lang:'English',
-        timestamp: '2019/7/21  1:07:53'
-    },
-    {
-      id: '70',
-      title:'A Very English Scandal',
-      akas: ["Egy nagyon angolos botr", "Un scandal britanic"],
-      genres: [ "Adventure", "Drama"],
-      image: [ "http://static.tvmaze.com/uploads/images/medium_portrait/164/411209.jpg", "http://static.tvmaze.com/uploads/images/original_untouched/164/411209.jpg"],
-      rating: 7,
-      lang:'English',
-      timestamp: '2019/7/21  1:07:53'
-    },
-    {
-        id: '80',
-        title:'A Very English Scandal',
-        akas: ["Egy nagyon angolos botr", "Un scandal britanic"],
-        genres: [  "Comedy" ],
-        image: [ "http://static.tvmaze.com/uploads/images/medium_portrait/164/411209.jpg", "http://static.tvmaze.com/uploads/images/original_untouched/164/411209.jpg"],
-        rating: 7,
-        lang:'Chinese',
-        timestamp: '2019/7/21  1:07:53'
-    },
-    {
-        id: '90',
-        title:'A Very English Scandal',
-        akas: ["Egy nagyon angolos botr", "Un scandal britanic"],
-        genres: [ "Adventure", "Drama"],
-        image: [ "http://static.tvmaze.com/uploads/images/medium_portrait/164/411209.jpg", "http://static.tvmaze.com/uploads/images/original_untouched/164/411209.jpg"],
-        rating: 7,
-        lang:'Chinese',
-        timestamp: '2019/7/21  1:07:53'
-    },
-    {
-      id: '190',
-      title:'A Very English Scandal',
-      akas: ["Egy nagyon angolos botr", "Un scandal britanic"],
-      genres: [ "Adventure", "Drama"],
-      image: [ "http://static.tvmaze.com/uploads/images/medium_portrait/164/411209.jpg", "http://static.tvmaze.com/uploads/images/original_untouched/164/411209.jpg"],
-      rating: 7,
-      lang:'Chinese',
-      timestamp: '2019/7/21  1:07:53'
-  },      {
-    id: '290',
-    title:'A Very English Scandal',
-    akas: ["Egy nagyon angolos botr", "Un scandal britanic"],
-    genres: [ "Adventure", "Drama"],
-    image: [ "http://static.tvmaze.com/uploads/images/medium_portrait/164/411209.jpg", "http://static.tvmaze.com/uploads/images/original_untouched/164/411209.jpg"],
-    rating: 7,
-    lang:'Chinese',
-    timestamp: '2019/7/21  1:07:53'
-},
-{
-  id: '390',
-  title:'A Very English Scandal',
-  akas: ["Egy nagyon angolos botr", "Un scandal britanic"],
-  genres: [ "Adventure", "Drama"],
-  image: [ "http://static.tvmaze.com/uploads/images/medium_portrait/164/411209.jpg", "http://static.tvmaze.com/uploads/images/original_untouched/164/411209.jpg"],
-  rating: 7,
-  lang:'Chinese',
-  timestamp: '2019/7/21  1:07:53'
-},
-{
-id: '490',
-title:'A Very English Scandal',
-akas: ["Egy nagyon angolos botr", "Un scandal britanic"],
-genres: [ "Adventure", "Drama"],
-image: [ "http://static.tvmaze.com/uploads/images/medium_portrait/164/411209.jpg", "http://static.tvmaze.com/uploads/images/original_untouched/164/411209.jpg"],
-rating: 7,
-lang:'Chinese',
-timestamp: '2019/7/21  1:07:53'
-},
-{
-id: '590',
-title:'A Very English Scandal',
-akas: ["Egy nagyon angolos botr", "Un scandal britanic"],
-genres: [ "Adventure", "Drama"],
-image: [ "http://static.tvmaze.com/uploads/images/medium_portrait/164/411209.jpg", "http://static.tvmaze.com/uploads/images/original_untouched/164/411209.jpg"],
-rating: 7,
-lang:'Chinese',
-timestamp: '2019/7/21  1:07:53'
-},
-{
-id: '690',
-title:'A Very English Scandal',
-akas: ["Egy nagyon angolos botr", "Un scandal britanic"],
-genres: [ "Adventure", "Drama"],
-image: [ "http://static.tvmaze.com/uploads/images/medium_portrait/164/411209.jpg", "http://static.tvmaze.com/uploads/images/original_untouched/164/411209.jpg"],
-rating: 7,
-lang:'Chinese',
-timestamp: '2019/7/21  1:07:53'
-},
-{
-id: '790',
-title:'A Very English Scandal',
-akas: ["Egy nagyon angolos botr", "Un scandal britanic"],
-genres: [ "Adventure", "Drama"],
-image: [ "http://static.tvmaze.com/uploads/images/medium_portrait/164/411209.jpg", "http://static.tvmaze.com/uploads/images/original_untouched/164/411209.jpg"],
-rating: 7,
-lang:'Chinese',
-timestamp: '2019/7/21  1:07:53'
+  }
+];
+var teststate1 = {
+  data: movies,
+  filter: {}
+}
+var teststate2 = {
+  data: movies,
+  filter: { lang: 'English' }
 }
 
-];
-teststate1={data:movies,
-            filter:{}}
-teststate2={data:movies,
-              filter:{lang:'English'}}
-describe('get movies', () => {
+describe('add filter action', () => {
 
+  it('add filter action1', () => {
+    expect(
+      MoviesReducer(teststate1, addFilter({ key: 'lang', value: 'English' })).filter
+    ).toEqual({ lang: 'English' })
+  })
+})
+
+describe('add filter action2', () => {
   it('should get movies', () => {
     expect(
-      MoviesReducer(teststate, {
-          type: 'addfilter',
-          payload: {lang:'English'}
-        }
-      ).payload.lang
-    ).toEqual('English')
-
-    expect(
-      MoviesReducer(teststate2, {
-          type: 'addfilter',
-          payload: {genres:'Drama'}
-        }
-      ).payload.lang
-    ).toEqual('English')
-
+      MoviesReducer(teststate2, addFilter({ key: 'genres', value: 'Drama' })).filter
+    ).toEqual({ lang: 'English', genres: 'Drama' })
   })
 })
 
