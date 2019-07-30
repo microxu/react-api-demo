d0c9b1cb4515679512fbc70966ca012c9af9685f
xu <yongchengxu@gmail.com>
2019-07-30 20:58:43 +1200

    no message

--- testclient/src/components/movieHome.js
+++ testclient/src/components/movieHome.js
@@ -4,23 +4,34 @@ import { HoCFilteritem } from './filters';
 
 import { Container, Row, Col, Image } from 'react-bootstrap';
 const style = {
-    top:'0',
-    position:'fixed'
-  };
+    "line-height": '60px',
+    background: 'Black',
+    position: 'fixed',
+    width: '100%',
+    top:'0'
+};
 
 export class MovieHome extends React.PureComponent {
 
     render() {
-        const eventKeys = [{eveKeys:["ALL", "English", "Chinese"],variant:"success"}, {eveKeys:["ALL", "Comedy", "Adventure", "Drama"],variant:"info"}, {eveKeys:["ALL", "8", "5", "3"],variant:"dark"}];
+        const eventKeys = [{ eveKeys: ["ALL", "English", "Chinese"], variant: "success" }, { eveKeys: ["ALL", "Comedy", "Adventure", "Drama"], variant: "info" }, { eveKeys: ["ALL", "8", "5", "3"], variant: "dark" }];
         return (
-            <Container>
-                <Row >
-                    <HoCFilteritem title="Language" name="lang" eventKeys={eventKeys[0]["eveKeys"]} variant={eventKeys[0]["variant"]}/>
-                    <HoCFilteritem title="genres" name="genres" eventKeys={eventKeys[1]["eveKeys"]} variant={eventKeys[1]["variant"]}/>
-                    <HoCFilteritem title="rating" name="rating" eventKeys={eventKeys[2]["eveKeys"]} variant={eventKeys[2]["variant"]}/>
-                </Row>
-                <HoCItems history={this.props.history} />
-            </Container>
+            <div style={{margin:0,padding:0}}>
+                <div style={style}>
+                    <div style={{ float: 'left' }}>
+                        <HoCFilteritem title="Language" name="lang" eventKeys={eventKeys[0]["eveKeys"]} variant={eventKeys[0]["variant"]} />
+                    </div>
+                    <div style={{ float: 'left' }}>
+                        <HoCFilteritem title="genres" name="genres" eventKeys={eventKeys[1]["eveKeys"]} variant={eventKeys[1]["variant"]} />
+                    </div>
+                    <div style={{ float: 'left' }}>
+                        <HoCFilteritem title="rating" name="rating" eventKeys={eventKeys[2]["eveKeys"]} variant={eventKeys[2]["variant"]} />
+                    </div>
+                </div>
+                <div style={{"margin-top":'60px'}}>
+                    <HoCItems history={this.props.history} />
+                </div>
+            </div>
         );
     }
 }
